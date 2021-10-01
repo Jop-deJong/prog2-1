85fac5bd75b164db7a5072c750e48d886ac8c06e
Jop-deJong <dejo030205@leerling.mosa-rt.be>
2021-10-01 09:07:12 +0200

    opgave016

---
+++ Oefeningen/11-/opgave016/opgave016.py
@@ -0,0 +1,10 @@
+print("""
+    <svg version="1.1"
+    baseProfile="full"
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg">
+    """)
+for circle in range(0,10):
+    print (' <circle cx="150" cy="100" r="80" fill="green"/> ')
+   
+print ("</svg>")
